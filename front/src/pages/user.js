53be7af99b4769acfd58a4d0b19f3599ae55db67
Hiroto Toyoda <>
2023-02-08 18:38:58 +0900

    Add: アカウントを閲覧するページでユーザ情報を取得するAPIを叩くようにする

--- front/src/pages/user.js
+++ front/src/pages/user.js
@@ -2,12 +2,32 @@ import React from 'react'
 import Sidebar from '../component/sidebar'
 import { UserInfo } from '../component/user_info'
 import TweetStyle from '../css/tweet.module.css';
+import { client } from '../libs/axios'
+import { useState } from 'react';
+import { useParams } from "react-router-dom";
 
 export const User = () => {
+    const token = localStorage.getItem('token')
+    const [user, setUser] = useState({});
+    const params = useParams();
+    window.onload = function () {
+		UserGet()
+	}
+    const UserGet = () => {
+        const url = 'v1/user/' + params.id
+		client
+			.get(url, { headers: { Authorization: token } })
+			.then((res) => {
+				setUser(res.data)
+			})
+			.catch((err) => {
+				console.log("err", err.response)
+			})
+	}
     return (
         <div className={TweetStyle.Tweet}>
             <Sidebar />
-            <UserInfo />
+            <UserInfo userName={user.name}/>
         </div>
     )
 }
