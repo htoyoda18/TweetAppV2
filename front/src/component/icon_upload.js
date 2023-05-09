dd092c95315b27de235a86c10a2922120214dbe9
Hiroto Toyoda <>
2023-05-09 22:44:56 +0900

    Add: プロフィールを編集するモーダルを作成

---
+++ front/src/component/icon_upload.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import sharedStyle from '../css/shared.module.css';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+
+export const ImageUploader = (props) => {
+    const [preview, setPreview] = useState(props.image);
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setPreview(URL.createObjectURL(file));
+        }
+    };
+
+    return (
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+            <input
+                type="file"
+                id="fileInput"
+                accept="image/*"
+                onChange={handleImageChange}
+                style={{ display: 'none' }}
+            />
+            <img
+                className={sharedStyle.largeIcon}
+                src={preview}
+                onClick={() => document.getElementById('fileInput').click()}
+                style={{ cursor: 'pointer' }}
+            />
+            <CameraAltIcon
+                onClick={() => document.getElementById('fileInput').click()}
+                style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    cursor: 'pointer',
+                    fontSize: 35,
+                }}
+            />
+        </div>
+    );
+};
+
+export default ImageUploader;
--- front/src/component/user_info.js
+++ front/src/component/user_info.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { LargeIcon } from '../component/shared'
+import { ImageUploader } from '../component/icon_upload'
 import UserInfoStyle from '../css/user_info.module.css';
+import Modal from "react-modal";
 
 export const UserInfo = (props) => {
   return (
     <div className={UserInfoStyle.userInfo}>
       <div className={UserInfoStyle.content}>
         <LargeIcon image="https://d38vrblg2ltm93.cloudfront.net/res/wonder-fe/user_id_46268/work/2021/10/10/image/20211010235446.png" />
-        <EditUserInfoBtn showUserId={props.userID}/>
+        <EditUserInfoBtn showUserId={props.userID} />
         <div className={UserInfoStyle.userName}>{props.userName}</div>
         <div className={UserInfoStyle.introduction}>自己紹介</div>
       </div>
@@ -17,7 +19,27 @@ export const UserInfo = (props) => {
 
 const EditUserInfoBtn = (showUserId) => {
   const [isSelf, setIsSelf] = useState(false);
-  const selfId =  parseInt(localStorage.getItem('userID'), 10);
+  const selfId = parseInt(localStorage.getItem('userID'), 10);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [introduction, setIntroduction] = useState('');
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handleIntroductionChange(event) {
+    setIntroduction(event.target.value);
+  }
+
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  function enableScroll() {
+    document.body.style.overflow = 'auto';
+  }
+
   useEffect(() => {
     if (selfId === showUserId.showUserId) {
       setIsSelf(true);
@@ -26,12 +48,40 @@ const EditUserInfoBtn = (showUserId) => {
     }
   }, [selfId, showUserId]);
 
+  // 次の時間は、yarn startをやり直して、エラーが出ないか？
   return (
     <div>
       {isSelf && (
-        <button className={UserInfoStyle.editUser} onClick={() => { /* プロフィール編集処理 */ }}>
-          プロフィールを編集
-        </button>
+        <div>
+          <button className={UserInfoStyle.editUser} onClick={() => { setModalIsOpen(true); disableScroll(); }}>
+            プロフィールを編集
+          </button>
+          <div>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => { setModalIsOpen(false); enableScroll(); }}
+              className={UserInfoStyle.modal}
+              overlayClassName={UserInfoStyle.modalOverlay}
+              style={{
+                content: {
+                  position: 'absolute',
+                  top: '30%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)'
+                },
+              }}
+            >
+              <button className={UserInfoStyle.modalClose} onClick={() => { setModalIsOpen(false); enableScroll(); }}>&times;</button>
+              <div className={UserInfoStyle.modalTitle}>プロフィールを編集</div>
+              <ImageUploader image="https://d38vrblg2ltm93.cloudfront.net/res/wonder-fe/user_id_46268/work/2021/10/10/image/20211010235446.png" />
+              <form>
+                <input className={UserInfoStyle.modalUsername} type="text" placeholder='ユーザ名' value={username} onChange={handleUsernameChange} />
+                <br />
+                <textarea className={UserInfoStyle.modalIntroduction} type="text" placeholder='自己紹介' value={introduction} onChange={handleIntroductionChange} />
+                <br />
+                <button className={UserInfoStyle.modalBtn} type="submit">保存</button>
+              </form>
+            </Modal>
+          </div>
+        </div>
       )}
     </div>
   );
