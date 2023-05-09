dc6b19a47dc45302b8aaa0332a41091d548333cc
Hiroto Toyoda <>
2023-05-10 01:04:10 +0900

    Add: iconカラムを追加

--- api/model/user.go
+++ api/model/user.go
@@ -4,6 +4,7 @@ type User struct {
 	ModelAt
 
 	ID       int    `json:"id"`
+	Icon     string `json:"icon"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"-"`
