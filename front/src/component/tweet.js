9009a1dde456b92d479922ecfca648ebbc442900
Hiroto Toyoda <>
2023-05-11 02:45:36 +0900

    Add: ホーム画面でアイコンを取得出来るようにする

--- front/src/component/tweet.js
+++ front/src/component/tweet.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CommentIcon from '@mui/icons-material/Comment';
 import TweetStyle from '../css/tweet_list.module.css';
 import { Icon } from "../component/shared"
 import { useNavigate } from 'react-router-dom';
+import { UserIconGet } from "../api/icon_get"
 
 export const Tweet = (props) => {
     // const likeCount = () => {
@@ -16,17 +17,32 @@ export const Tweet = (props) => {
     //         return props.replies.length
     //     }
     // }
+    const [iconUrl, setIconUrl] = useState("");
     const navigate = useNavigate();
     const handleClick = () => {
         const url = '/tweet_detail/' + props.id
         navigate(url);
     }
+
+    useEffect(() => {
+			const getIconUrl = async () => {
+				if (props.image !== "") {
+					try {
+						const url = await UserIconGet(props.image);
+						setIconUrl(url);
+					} catch (err) {
+						console.log("err", err.response);
+					}
+				}
+			};
+			getIconUrl();
+		}, [props.image]);
     return (
         <div className={TweetStyle.Tweet} onClick={handleClick}>
             <div className={TweetStyle.tweetContent}>
                     <form>
                         <div className={TweetStyle.Section1}>
-                            <Icon image={props.image} userID={props.userID} />
+                            <Icon image={iconUrl} userID={props.userID} />
                             <div className={TweetStyle.content}>
                                 <div className={TweetStyle.userName}>{props.userName}</div>
                                 <div className={TweetStyle.tweet}>{props.tweet}</div>
--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Sidebar from '../component/sidebar';
 import TweetListStyle from '../css/tweet_list.module.css';
 import { Tweet } from "../component/tweet";
+import { client } from '../libs/axios'
 
 export const Home = () => {
 	const [tweets, setTweets] = useState([]);
@@ -15,34 +16,40 @@ export const Home = () => {
 			return;
 		}
 
-		const fetchData = async () => {
-			const response = await fetch('http://localhost:8080/v1/tweet', {
-				headers: {
-					Authorization: token,
-				},
-			});
-			const res = await response.json();
-			if (!response.ok) {
-				if (res === 'Fail auth token') { // エラーメッセージを確認する
-					navigate('/login');
-					return;
-				}
-			}
-			setTweets(res);
-		};
+		const TweetList = () => {
+			client
+				.get('v1/tweet', { headers: { Authorization: token } })
+				.then(async (res) => {
+					if (res.data) {
+						setTweets(res.data);
+					}
+					if (!res.ok) {
+						if (res === 'Fail auth token') {
+							navigate('/login');
+							return;
+						}
+					}
+				})
+		}
 
-		fetchData();
+		TweetList();
 	}, [token, navigate]);
 
 	return (
 		<div className={TweetListStyle.TweetList}>
 			<Sidebar />
 			<div className={TweetListStyle.Tweet}>
-				{tweets.map((value, key) => {
-					return (
-						<Tweet userID={value.user.id} id={value.id} userName={value.user.name} tweet={value.tweet} replies={value.replies} likes={value.like} image='https://sp-akiba-souken.k-img.com/images/vote/000/170/170628.jpg' />
-					)
-				})}
+				{tweets.map((value, key) => (
+					<Tweet
+					userID={value.user.id}
+					id={value.id}
+					userName={value.user.name}
+					tweet={value.tweet}
+					replies={value.replies}
+					likes={value.like}
+					image={value.user.icon}
+				/>
+				))}
 			</div>
 		</div>
 	);
