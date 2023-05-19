eea8fd72c3b98c3b83a9fcca81234a4fbda358b0
Hiroto Toyoda <>
2023-05-19 21:04:53 +0900

    Add: タブのアイコンを指定

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../css/global.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <link rel="icon" href={'./tab_icon.jpeg'} />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    )
 }
 
 export default MyApp;
