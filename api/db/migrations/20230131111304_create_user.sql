8b7ad86f409cc5ad9fe49e8cf96977a7e0e64922
Hiroto Toyoda <>
2023-05-25 17:45:06 +0900

    Fix: ユーザテーブルがマイグレーションされる際に、ハッシュ化したパスワードを入れるようにする

--- api/db/migrations/20230131111304_create_user.sql
+++ api/db/migrations/20230131111304_create_user.sql
@@ -15,11 +15,11 @@ CREATE TABLE `users` (
 INSERT INTO 
     `users` (`name`, `email`, `icon`, `introduction`, `password`, `created_at`, `updated_at`) 
 VALUES
-    ('ウィスパー', '1@example.com', 'ウィスパー.png', 'ニョロロン族 / ウワノソラ族。「190年前、正義を気取った僧により悪者とされ封印された“妖怪執事”」を自称する妖怪。', 'hogehoge', NOW(), NOW()), 
-    ('ジバニャン', '2@example.com', 'ジバニャン.png', 'プリチー族 / モノノケ族。トラックに轢かれて死んだ猫が成仏できず、地縛霊となったの赤色のネコ妖怪', 'hogehoge', NOW(), NOW()), 
-    ('USAピョン', '3@example.com', 'USAピョン.png', 'ウスラカゲ族 / ウワノソラ族。ゲーム版『3』とアニメ版のセカンドシーズンから登場するメリケン妖怪。', 'hogehoge', NOW(), NOW()), 
-    ('コマさん', '4@example.com', 'コマさん.png', 'プリチー族 / オマモリ族。神社の狛犬に憑依した妖怪。一人称は「オラ」で、「〜ズラ」、「もんげー」（岡山弁で「ものすごい」という意味）が口癖。', 'hogehoge', NOW(), NOW()), 
-    ('コマじろう', '5@example.com', 'コマじろう.png', 'プリチー族 / オマモリ族。コマさんの双子の弟。', 'hogehoge', NOW(), NOW());
+    ('ウィスパー', '1@example.com', 'ウィスパー.png', 'ニョロロン族 / ウワノソラ族。「190年前、正義を気取った僧により悪者とされ封印された“妖怪執事”」を自称する妖怪。', '$2a$10$RQksNR0wn9SzOkVwXyfG2eotJ6ckENaUBI8qpMZSRcZIcbYnT/l12', NOW(), NOW()), 
+    ('ジバニャン', '2@example.com', 'ジバニャン.png', 'プリチー族 / モノノケ族。トラックに轢かれて死んだ猫が成仏できず、地縛霊となったの赤色のネコ妖怪', '$2a$10$RQksNR0wn9SzOkVwXyfG2eotJ6ckENaUBI8qpMZSRcZIcbYnT/l12', NOW(), NOW()), 
+    ('USAピョン', '3@example.com', 'USAピョン.png', 'ウスラカゲ族 / ウワノソラ族。ゲーム版『3』とアニメ版のセカンドシーズンから登場するメリケン妖怪。', '$2a$10$RQksNR0wn9SzOkVwXyfG2eotJ6ckENaUBI8qpMZSRcZIcbYnT/l12', NOW(), NOW()), 
+    ('コマさん', '4@example.com', 'コマさん.png', 'プリチー族 / オマモリ族。神社の狛犬に憑依した妖怪。一人称は「オラ」で、「〜ズラ」、「もんげー」（岡山弁で「ものすごい」という意味）が口癖。', '$2a$10$RQksNR0wn9SzOkVwXyfG2eotJ6ckENaUBI8qpMZSRcZIcbYnT/l12', NOW(), NOW()), 
+    ('コマじろう', '5@example.com', 'コマじろう.png', 'プリチー族 / オマモリ族。コマさんの双子の弟。', '$2a$10$RQksNR0wn9SzOkVwXyfG2eotJ6ckENaUBI8qpMZSRcZIcbYnT/l12', NOW(), NOW());
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
