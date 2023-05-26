c1db449487207c7231ded69de72dedcd0657e204
Hiroto Toyoda <>
2023-05-26 23:03:00 +0900

    Fix: TearDown関数からエラーを返すようにする

--- api/shared/test/tear_down.go
+++ api/shared/test/tear_down.go
@@ -6,18 +6,23 @@ import (
 	"gorm.io/gorm"
 )
 
-func TearDown(db *gorm.DB) {
+func TearDown(db *gorm.DB) error {
 	sqlDB, err := db.DB()
 	if err != nil {
 		shared.Error("TearDown: db.DB()", err)
+		return err
 	}
 
 	goose.SetDialect("mysql")
 	if err := goose.DownTo(sqlDB, "../../db/migrations", 0); err != nil {
 		shared.Error("goose down failed: %v", err)
+		return err
 	}
 
 	if err := goose.Up(sqlDB, "../../db/migrations"); err != nil {
 		shared.Error("goose up failed: %v", err)
+		return err
 	}
+
+	return nil
 }
