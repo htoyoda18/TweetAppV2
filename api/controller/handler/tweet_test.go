5f76e11bd7ffa3130ae89acbd49fad160e2432f4
Hiroto Toyoda <>
2023-06-10 19:17:50 +0900

    ref: ツイート作成のテストを追加

---
+++ api/controller/handler/tweet_test.go
@@ -0,0 +1,42 @@
+package handler_test
+
+import (
+	"testing"
+
+	"github.com/htoyoda18/TweetAppV2/api/controller/handler/request"
+	"github.com/htoyoda18/TweetAppV2/api/shared"
+	"github.com/htoyoda18/TweetAppV2/api/shared/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTweetCreate(t *testing.T) {
+	tests := []struct {
+		name          string
+		body          request.Tweet
+		responseError error
+	}{
+		{
+			name:          "成功:ツイートを投稿する",
+			body:          request.Tweet{Tweet: "test tweet"},
+			responseError: nil,
+		},
+		{
+			name:          "失敗:ツイートが空",
+			body:          request.Tweet{Tweet: ""},
+			responseError: shared.ShouldBindJsonErr,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			statusCode, result := test.APIClientForPost(tt.body, "tweet", token)
+			if statusCode == 200 {
+				assert.Equal(t, tt.responseError, nil)
+			} else {
+				errMsg, _ := test.ReadErrorResponse(result)
+				assert.Equal(t, tt.responseError.Error(), errMsg)
+			}
+		})
+	}
+	test.TearDown(gormDB)
+}
