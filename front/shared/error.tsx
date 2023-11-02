cac5063d5b0d39afc8eed7e91ff986c872304cab
Hiroto Toyoda <>
2023-11-02 22:31:22 +0900

    fix: フロント側でもバリデーションをかけるようにする

--- front/shared/error.tsx
+++ front/shared/error.tsx
@@ -1,18 +1,19 @@
 export const ApiErrorMessages = {
-    UserNotFound: 'User not found',
-    UserEmailDuplicate: 'User email duplicate',
-    EmailNotFound: 'Email not found',
-    ShouldBindJsonErr: 'Should bind JSON error',
-    FailToParse: 'Fail To Parse',
-    FileNotFound: 'File Not Found',
-    FileNotOpen: 'File Not Open',
-    FailAuthToken: 'Fail auth token',
-    FailPassword: 'Fail password',
-    DuplicateLike: 'DuplicateLike',
-    RecordNotFound: 'record not found',
-    TokenIsExpired: 'Token is expired'
+  UserNotFound: "User not found",
+  UserEmailDuplicate: "User email duplicate",
+  EmailNotFound: "Email not found",
+  ShouldBindJsonErr: "Should bind JSON error",
+  InvalidPassword: "Invalid password",
+  FailToParse: "Fail To Parse",
+  FileNotFound: "File Not Found",
+  FileNotOpen: "File Not Open",
+  FailAuthToken: "Fail auth token",
+  FailPassword: "Fail password",
+  DuplicateLike: "DuplicateLike",
+  RecordNotFound: "record not found",
+  TokenIsExpired: "Token is expired",
 };
 
 export const FrontErrorMessages = {
-    TokenNotFound: 'Token is not found'
-}
+  TokenNotFound: "Token is not found",
+};
